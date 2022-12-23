a452c4adbc94627b9e0e664b34d4e148017da30b
Momena <momenasukkar@gmail.com>
2022-12-23 14:20:02 +0300

    new edits

--- ALTANMYA_Attendence_Payroll_System/models/project.py
+++ ALTANMYA_Attendence_Payroll_System/models/project.py
@@ -1,5 +1,5 @@
 from odoo import api, models, fields, _
-
+from datetime import datetime, timedelta
 from odoo.exceptions import UserError, ValidationError
 
 
@@ -36,10 +36,68 @@ class ProjectTaskInherited(models.Model):
     no_repeated_errors = fields.Selection([('1', '1'), ('2', '2'), ('3', '3'),
                                            ('4', '4'), ('5', '5')], string='No Repeated Error')
     requested_by = fields.Many2many('res.users', readonly=True, related='user_ids', string="Requested By")
-    # description = fields.Html(required=True)
+    department_id = fields.Many2one('hr.department', compute="_compute_department_id", store=True)
+    description = fields.Html(required=True)
+    planned_date_from = fields.Date("Start date")
+    planned_date_to = fields.Date("End date")
 
-    # @api.constrains('description')
-    # def _check_len_html(self):
-    #     print(self.description, len(self.description))
-    #     if len(self.description) < 25 + 7:
-    #         raise ValidationError("Description should be more than 25 character")
+    planned_date_begin = fields.Datetime("Start date", tracking=True, task_dependency_tracking=True,
+                                         compute="_compute_planned_date_begin")
+    planned_date_end = fields.Datetime("End date", compute="_compute_planned_date_end")
+
+    @api.depends('planned_date_from')
+    def _compute_planned_date_begin(self):
+        for rec in self:
+            if rec.planned_date_from:
+                date = rec.planned_date_from
+                rec.planned_date_begin = datetime(date.year, date.month, date.day, 21, 0, 0) - timedelta(
+                    days=1)
+            else:
+                rec.planned_date_begin = False
+
+    @api.depends('planned_date_to')
+    def _compute_planned_date_end(self):
+        for rec in self:
+            if rec.planned_date_to:
+                date = rec.planned_date_to
+                rec.planned_date_end = datetime(date.year, date.month, date.day, 21, 0, 0) - timedelta(days=1)
+            else:
+                rec.planned_date_end = False
+
+    @api.constrains('description')
+    def _check_len_html(self):
+        print(self.description, len(self.description))
+        if len(self.description) < 25 + 7:
+            raise ValidationError("Description should be more than 25 character")
+
+    @api.depends('user_ids')
+    def _compute_department_id(self):
+        for rec in self:
+            rec.department_id = False
+            if rec.user_ids:
+                for user_id in rec.user_ids:
+                    if user_id.employee_id:
+                        if user_id.employee_id.department_id:
+                            rec.department_id = user_id.employee_id.department_id.id
+                            break
+
+
+class account_analytic_line_inherited(models.Model):
+    _inherit = 'account.analytic.line'
+
+    @api.constrains('name')
+    def _check_name_len(self):
+        if len(self.name) < 25:
+            raise ValidationError(
+                "The Description of timesheet line cannot be empty and should be more than 25 characters.")
+
+
+class ReportProjectTaskUserInherited(models.Model):
+    _inherit = "report.project.task.user"
+    department_id = fields.Many2one('hr.department', readonly=True, string="Department")
+
+    def _select(self):
+        return super()._select() + ", t.department_id"
+
+    def _group_by(self):
+        return super()._group_by() + ", t.department_id"
