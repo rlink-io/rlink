0f314348f0a8789eb922bd36326e01fee15ab80a
rlink-io <106970994+rlink-io@users.noreply.github.com>
2023-11-11 20:34:42 +0300

    Update project_inherited.py

--- RLink_Project_Edits/models/project_inherited.py
+++ RLink_Project_Edits/models/project_inherited.py
@@ -27,3 +27,39 @@ class ProjectInherited(models.Model):
         type_ids = [to_do.id, doing.id, to_check.id, done.id]
         rec.type_ids = type_ids
         return rec
+
+    def write(self, vals):
+        # directly compute is_favorite to dodge allow write access right
+        if 'is_favorite' in vals:
+            vals.pop('is_favorite')
+            self._fields['is_favorite'].determine_inverse(self)
+        res = super(ProjectInherited, self).write(vals) if vals else True
+        if 'user_id' in vals:
+            self.env['res.users'].sudo().browse(vals['user_id']).write({
+                     'groups_id': [(4, self.env.ref('project.group_project_manager').id)]
+                })
+
+        if 'allow_recurring_tasks' in vals and not vals.get('allow_recurring_tasks'):
+            self.env['project.task'].search([('project_id', 'in', self.ids), ('recurring_task', '=', True)]).write({'recurring_task': False})
+
+        if 'active' in vals:
+            # archiving/unarchiving a project does it on its tasks, too
+            self.with_context(active_test=False).mapped('tasks').write({'active': vals['active']})
+        if vals.get('partner_id') or vals.get('privacy_visibility'):
+            for project in self.filtered(lambda project: project.privacy_visibility == 'portal'):
+                project.message_subscribe(project.partner_id.ids)
+        if vals.get('privacy_visibility'):
+            self._change_privacy_visibility()
+        if 'name' in vals and self.analytic_account_id:
+            projects_read_group = self.env['project.project'].read_group(
+                [('analytic_account_id', 'in', self.analytic_account_id.ids)],
+                ['analytic_account_id'],
+                ['analytic_account_id']
+            )
+            analytic_account_to_update = self.env['account.analytic.account'].browse([
+                res['analytic_account_id'][0]
+                for res in projects_read_group
+                if res['analytic_account_id'] and res['analytic_account_id_count'] == 1
+            ])
+            analytic_account_to_update.write({'name': self.name})
+        return res
