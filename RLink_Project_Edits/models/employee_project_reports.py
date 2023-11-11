0612d8ae17b091f2d405a0e6a5c495a1581a6062
rlink-io <106970994+rlink-io@users.noreply.github.com>
2023-11-11 22:07:47 +0300

    Update employee_project_reports.py

--- RLink_Project_Edits/models/employee_project_reports.py
+++ RLink_Project_Edits/models/employee_project_reports.py
@@ -4,6 +4,7 @@ from odoo.exceptions import UserError, ValidationError
 import locale
 import logging
 from dateutil.relativedelta import relativedelta
+
 _logger = logging.getLogger(__name__)
 
 locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
@@ -46,27 +47,30 @@ class ProjectEmployeesReports(models.Model):
                 if user_id in task.user_ids.ids \
                         and task.date_check.month == yesterday_date.month \
                         and task.date_check.year == yesterday_date.year:
-                    task_number = task_number + 1
-                    task.task_number = task_number
-                    task.total = ((int(task.speed) * 1.5) + (int(task.quality) * 2) + (int(task.no_repeated_errors))) / 4.5
-                    sum_total += task.total
-                    task_ids.append(task.id)
+                            task_number = task_number + 1
+                            task.task_number = task_number
+                            task.total = ((int(task.speed) * 1.5) + (int(task.quality) * 2) + (
+                                int(task.no_repeated_errors))) / 4.5
+                            sum_total += task.total
+                            task_ids.append(task.id)
             elif not task.date_check:
                 if task.planned_date_to:
-                     if user_id in task.user_ids.ids \
-                        and task.planned_date_to.month == yesterday_date.month \
-                        and task.planned_date_to.year == yesterday_date.year:
-                    task_number = task_number + 1
-                    task.task_number = task_number
-                    task.total = ((int(task.speed) * 1.5) + (int(task.quality) * 2) + (int(task.no_repeated_errors))) / 4.5
-                    sum_total += task.total
-                    task_ids.append(task.id)
+                    if user_id in task.user_ids.ids \
+                            and task.planned_date_to.month == yesterday_date.month \
+                            and task.planned_date_to.year == yesterday_date.year:
+                                task_number = task_number + 1
+                                task.task_number = task_number
+                                task.total = ((int(task.speed) * 1.5) + (int(task.quality) * 2) + (
+                                    int(task.no_repeated_errors))) / 4.5
+                                sum_total += task.total
+                                task_ids.append(task.id)
         if task_ids:
             monthly_total = sum_total / len(task_ids)
 
         return task_ids, monthly_total
-    def compute_task_ids_month(self, user_id,i):
-        
+
+    def compute_task_ids_month(self, user_id, i):
+
         yesterday_date = date.today() - relativedelta(months=i)
         done_stages = self.env['project.task.type'].search([('name', '=', 'Done')]).ids
         all_task = self.env['project.task'].search([('stage_id', 'in', done_stages)])
@@ -81,13 +85,15 @@ class ProjectEmployeesReports(models.Model):
                         and task.planned_date_to.year == yesterday_date.year:
                     task_number = task_number + 1
                     task.task_number = task_number
-                    task.total = ((int(task.speed) * 1.5) + (int(task.quality) * 2) + (int(task.no_repeated_errors))) / 4.5
+                    task.total = ((int(task.speed) * 1.5) + (int(task.quality) * 2) + (
+                        int(task.no_repeated_errors))) / 4.5
                     sum_total += task.total
                     task_ids.append(task.id)
         if task_ids:
             monthly_total = sum_total / len(task_ids)
 
         return task_ids, monthly_total
+
     def _create_monthly_project_employee_report_cron1(self):
 
         all_users = self.env['res.users'].sudo().search([('share', '=', False)])
@@ -105,18 +111,18 @@ class ProjectEmployeesReports(models.Model):
                 'total': monthly_total
             }
             self.sudo().fill_Kpi_in_employees_reports(user, yesterday_date, monthly_total)
-            _logger.info(f'aqqqqqqqqqqqqqqqqqqq{self.sudo().fill_Kpi_in_employees_reports(user, yesterday_date, monthly_total)}')
+            _logger.info(
+                f'aqqqqqqqqqqqqqqqqqqq{self.sudo().fill_Kpi_in_employees_reports(user, yesterday_date, monthly_total)}')
 
             self.env['project.employees.reports'].sudo().create(vals)
 
-
     def _create_monthly_project_employee_report_cron1_month(self):
         all_users = self.env['res.users'].sudo().search([('share', '=', False)])
-        
-        for i in range(1,10):
-            yesterday_date = date.today() - relativedelta(months=i)           
+
+        for i in range(1, 10):
+            yesterday_date = date.today() - relativedelta(months=i)
             for user in all_users:
-                task_ids, monthly_total = self.sudo().compute_task_ids_month(user.id,i)
+                task_ids, monthly_total = self.sudo().compute_task_ids_month(user.id, i)
                 vals = {
                     'user_id': user.id,
                     'month': str(yesterday_date.month),
@@ -125,12 +131,9 @@ class ProjectEmployeesReports(models.Model):
                     'total': monthly_total
                 }
                 self.sudo().fill_Kpi_in_employees_reports(user, yesterday_date, monthly_total)
-                
-    
-                self.env['project.employees.reports'].sudo().create(vals)
-                
 
-               
+                self.env['project.employees.reports'].sudo().create(vals)
+
     def fill_Kpi_in_employees_reports(self, user, yesterday_date, monthly_total):
         if user.employee_ids:
             for employee_id in user.employee_ids:
